37042bfd0212c3e1ae37306d7c762048919b0c2a
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-07-02 21:27:50 +0200

    Update the TokyoNight-Day background color
    The TokyoNight Day variant has a new
    background color.
    https://github.com/folke/tokyonight.nvim/commit/5598215fa06572048bc857c9c71378a5433ec070

--- lib/irb/theme/tokyonight/day.rb
+++ lib/irb/theme/tokyonight/day.rb
@@ -1,5 +1,5 @@
 Reline::Face.config(:completion_dialog) do |conf|
   conf.define(:default, foreground: "#3760bf", background: "#e1e2e7")
-  conf.define(:enhanced, foreground: "#3760bf", background: "#b6bfe2")
+  conf.define(:enhanced, foreground: "#3760bf", background: "#b7c1e3")
   conf.define(:scrollbar, foreground: "#e9e9ec", background: "#2e7de9")
 end
